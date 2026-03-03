ee3b80b3d1007286c39e202484ad4fc368012109
Projects repositories of Professor Salinet (SENAC) <professorsalinet@gmail.com>
2026-03-02 22:00:15 -0300

    Add files via upload

--- Forca.java
+++ Forca.java
@@ -8,54 +8,66 @@ public class Forca {
         String respostaUsuario;
         ArrayList<String> letrasJogadas = new ArrayList<>();
         String palavraSorteada = palavra[2];
+        String letrasRestantes = palavraSorteada;
         int tamanhoPalavra = palavraSorteada.length();
         int letrasFaltando = palavraSorteada.length();
         String caracterCoringa = "_ ";
         String dicaPalavra = "";
-        for (int i = 0; i < tamanhoPalavra; i++) {
-            dicaPalavra += caracterCoringa;
-        }
         int letrasAcertadas = 0;
+        int letrasErradas = 0;
         String caracterAtual = caracterCoringa;
         String letraVerificada = "";
+        int numeroTentativas = 7;
 
-        while (acertouPalavra == false) {
-            System.out.println("Bem vindo ao jogo de Forca!");
+        System.out.println("Bem vindo ao jogo de Forca!");
+        System.out.println("Você ainda tem " + numeroTentativas + " tentativa(s).");
+        while (sair == false) {
+            if (numeroTentativas == 0) {
+                System.out.println("Você perdeu e alcançou o número máximo de tentativas! Tente outra vez...");
+                break;
+            }
 
             dicaPalavra = "";
-            System.out.println("letrasJogadas.toString(): " + letrasJogadas.toString());
             for (int i = 0; i < palavraSorteada.length(); i++) {
                 caracterAtual = caracterCoringa;
                 letraVerificada = String.valueOf(palavraSorteada.charAt(i));
-                // System.out.println("letraVerificada: " + letraVerificada);
                 for (int l = 0; l < letrasJogadas.size(); l++) {
                     if (letraVerificada.equals(letrasJogadas.get(l))) {
                         caracterAtual = letraVerificada;
-                        System.out.println("achou: " + letraVerificada);
                     }
                 }
-                dicaPalavra += caracterAtual + "";
+                dicaPalavra += caracterAtual + " ";
             }
 
-            System.out.println("Dica: a palavra: " + dicaPalavra + " tem " + tamanhoPalavra + " letras.");
-            System.out.println("Digite uma palavra e tecle Enter para jogar.");
+            System.out.println("Dica: a palavra: " + dicaPalavra + " tem " + tamanhoPalavra + " letra(s).");
+            System.out.println("Digite uma letra e tecle Enter para jogar.");
             respostaUsuario = inputScanner.nextLine().trim().toLowerCase();
             letrasJogadas.add(respostaUsuario);
             System.out.println("Você jogou a(s) letra(s): " + letrasJogadas.toString());
             
-            if (palavraSorteada.indexOf(respostaUsuario) > -1) {
-                palavraSorteada = palavraSorteada.replace(respostaUsuario, "");
+            if (letrasRestantes.indexOf(respostaUsuario) > -1) {
+                letrasRestantes = letrasRestantes.replace(respostaUsuario, "");
                 letrasAcertadas++;
                 letrasFaltando = tamanhoPalavra - letrasAcertadas;
+            } else {
+                letrasErradas++;
             }
-            System.out.println("Você acertou " + letrasAcertadas + " letras.");
-            
+            System.out.println("Você acertou " + letrasAcertadas + " letra(s).");
+            System.out.println("Você errou " + letrasErradas + " letra(s).");
+            System.out.println("Faltam " + letrasFaltando + " letra(s) pra você acertar.");
+
+            if (letrasRestantes.length() == 0) {
+                System.out.println("Você venceu, Parabéns!");
+                break;
+            }
+
             System.out.println("Continuar?");
             System.out.println("Digite s para sim e n para não, e tecle Enter");
             respostaUsuario = inputScanner.nextLine().trim().toLowerCase();
             if (respostaUsuario.equals("n")) {
                 sair = true;
             }
+            numeroTentativas--;
         }
         inputScanner.close();
     }
