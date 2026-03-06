2ee2a92adc72a4a251f418f9ef918024072c94fd
Projects repositories of Professor Salinet (SENAC) <professorsalinet@gmail.com>
2026-03-06 20:04:12 -0300

    Add files via upload

--- While.java
+++ While.java
@@ -1,18 +1,23 @@
 import java.util.*;
 public class While {
-    public static void main(String[] args) {
-        boolean sair = false;
+    public static void main (String[] args) {
         Scanner inputScanner = new Scanner(System.in);
-        String respostaUsuario = "";
-        int contador = 0;
+        String respostaUsuario;
+        Boolean sair = false;
         while (sair == false) {
-            System.out.println("Tentativa número " + contador);
-            System.out.println("Digite sair para sair do sistema ou c para continuar, depois tecle Enter");
+            System.out.println("Digite o primeiro número a ser somado e tecle Enter");
+            respostaUsuario = inputScanner.nextLine().trim();
+            int numeroUm = Integer.valueOf(respostaUsuario);
+            System.out.println("Digite o segundo número a ser somado e tecle Enter");
+            respostaUsuario = inputScanner.nextLine().trim();
+            int numeroDois = Integer.valueOf(respostaUsuario);
+            System.out.println("O resultado da soma é: " + (numeroUm + numeroDois));
+            System.out.println("Deseja realizar outra soma?");
+            System.out.println("Digite s para sim ou n para não, e tecle Enter");
             respostaUsuario = inputScanner.nextLine().trim().toLowerCase();
-            if (respostaUsuario.equals("s")) {
+            if (respostaUsuario.equals("n")) {
                 sair = true;
             }
-            contador++;
         }
         inputScanner.close();
     }
