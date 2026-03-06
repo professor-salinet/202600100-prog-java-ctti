b371cea63e7913045c9025a10aa083a199671ebd
Projects repositories of Professor Salinet (SENAC) <professorsalinet@gmail.com>
2026-03-06 17:33:54 -0300

    Add files via upload

--- ForEach.java
+++ ForEach.java
@@ -1,7 +1,8 @@
 public class ForEach {
     public static void main (String[] args) {
         String[] frutas = {"Banana","Laranja","Maçã","Pera","Uva"};
-        for(String fruta : frutas) {
+        // for (int i = 0; i < frutas.length; i++) {
+        for (String fruta : frutas) {
             System.out.println("A fruta é: " + fruta);
         }
     }
